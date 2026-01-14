37de2fb0e6e3d1a28cb4761837edb779a8b6f6df
223199 <162702161+223199@users.noreply.github.com>
2026-01-15 08:29:39 +0900

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import "./styles.css";
 import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <HashRouter>
+       <App />
+    </HashRouter>
   </React.StrictMode>
 );
